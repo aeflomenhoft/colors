7446cc499df6bddb26d277c7b6e2d45e1afb0fba
aeflomenhoft <91565743+aeflomenhoft@users.noreply.github.com>
2021-10-04 15:03:01 -0500

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,15 @@
+#Initialization
+import turtle
+import random
+ally=turtle.Turtle()
+
+
+#Functions
+def randomDot():
+    ally.color("blue")
+    ally.begin_fill()
+    ally.circle(150)
+    ally.end_fill()
+
+#Main
+randomDot()
